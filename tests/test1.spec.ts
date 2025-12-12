c4752310acdad7e94eca4147a345100dbce2b9bf
Hardik <ce.hardikdave@gmail.com>
2025-12-12 22:09:59 +0530

    This is first commit for git

---
+++ tests/test1.spec.ts
@@ -0,0 +1,25 @@
+import {test, expect} from "@playwright/test";
+
+
+test.beforeEach(async({page})=>{
+    await page.goto('https://www.google.com/')
+    console.log('Launch the URL')
+})
+
+
+test.describe('Test Suite', ()=>{
+
+    test.beforeEach(async({page})=>{
+
+        const txtsearch = await page.locator('.gLFyf')
+        expect(txtsearch).toBeEditable()
+        console.log('Assertion to check the editable search')
+    })
+
+    test('Enter the playwright and search',async({page})=>{
+        
+        await page.locator('.gLFyf').fill('Playwright')
+        await page.locator('.gNO89b').first().click()
+
+    })
+})
