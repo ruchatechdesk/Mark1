52618a4abe872bd870d43bf1e98e4f75abaa3a2a
Tech Desk <ruchatechdesk@gmail.com>
2025-12-15 18:27:58 +0530

    Added new test

---
+++ tests/test2.spec.ts
@@ -0,0 +1,20 @@
+import {test, expect} from "@playwright/test";
+
+test('Goto PW site and search', async({page})=>{
+
+    await page.goto('https://playwright.dev/')
+    console.log('URL opened')
+    const playwrightlogo  = await page.getByText('API')
+    await expect(playwrightlogo).toBeTruthy()
+
+
+    const playwrightsearch = await page.locator('.DocSearch-Button-Placeholder')
+    await expect(playwrightsearch).toBeVisible({ timeout: 5000 })
+    playwrightsearch.click()
+
+    const playwrightinput = await page.locator('.DocSearch-Input')
+    await expect(playwrightinput).toBeVisible({ timeout: 10000 });
+    await expect(playwrightinput).toBeVisible()
+    // playwrightinput.click()
+    await playwrightinput.fill('input')
+})
